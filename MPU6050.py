7e809187a0e45b78f497c7fd85ff2e9b3fe627db
Sammy Haq <sammy.haq1@gmail.com>
2018-06-19 14:27:14 -0400

    Changed everything to be adherent to PEP8 style

--- MPU6050.py
+++ MPU6050.py
@@ -15,102 +15,102 @@ import time
 """
 MPU6050 registers and addresses.
 """
-PWR_MGMT_1 = 0x6b;
-PWR_MGMT_2 = 0x6c;
+PWR_MGMT_1 = 0x6b
+PWR_MGMT_2 = 0x6c
 
 
-ACCEL_XOUT_H = 0x3b;
-ACCEL_XOUT_L = 0x3c;
+ACCEL_XOUT_H = 0x3b
+ACCEL_XOUT_L = 0x3c
 
-ACCEL_YOUT_H = 0x3d;
-ACCEL_YOUT_L = 0x3e;
+ACCEL_YOUT_H = 0x3d
+ACCEL_YOUT_L = 0x3e
 
-ACCEL_ZOUT_H = 0x3f;
-ACCEL_ZOUT_L = 0x40;
-
-
-TEMP_OUT_H = 0x41;
-TEMP_OUT_L = 0x42;
+ACCEL_ZOUT_H = 0x3f
+ACCEL_ZOUT_L = 0x40
 
 
-GYRO_XOUT_H = 0x43;
-GYRO_XOUT_L = 0x44;
-
-GYRO_YOUT_H = 0x45;
-GYRO_YOUT_L = 0x46;
-
-GYRO_ZOUT_H = 0x47;
-GYRO_ZOUT_L = 0x48;
+TEMP_OUT_H = 0x41
+TEMP_OUT_L = 0x42
 
 
-SMPLRT_DIV = 0x19; 
-CONFIG = 0x1a;
+GYRO_XOUT_H = 0x43
+GYRO_XOUT_L = 0x44
 
-WHO_AM_I_MPU6050 = 0x75; # should return 0x68 if visible.
+GYRO_YOUT_H = 0x45
+GYRO_YOUT_L = 0x46
+
+GYRO_ZOUT_H = 0x47
+GYRO_ZOUT_L = 0x48
+
+
+SMPLRT_DIV = 0x19
+CONFIG = 0x1a
+
+WHO_AM_I_MPU6050 = 0x75  # should return 0x68 if visible.
 
 # In older raspberry pi's, you may have to put 0 as the arg instead
-bus = smbus.SMBus(1);
+bus = smbus.SMBus(1)
+
 
 class MPU6050:
 
-    
     # Constructor.
     def __init__(self, address):
 
-         # use i2cdetect -y 1 to find the address of the chip. Example: for me, it's usually 0x68.
-        self.address = address;
+        # use i2cdetect -y 1 to find the address of the chip.
+        # Example: for me, it's usually 0x68.
+        self.address = address
 
-        bus.write_byte_data(address, PWR_MGMT_1, 0); # ensures power wakeup from sleep
-        bus.write_byte_data(address, SMPLRT_DIV, 7); # delays sample rate to avoid noise
-        bus.write_byte_data(address, CONFIG, 0); # disables DLPF.
-
+        # ensures power wakeup from sleep
+        bus.write_byte_data(address, PWR_MGMT_1, 0)
+        # delays sample rate to avoid noise
+        bus.write_byte_data(address, SMPLRT_DIV, 7)
+        bus.write_byte_data(address, CONFIG, 0)  # disables DLPF.
 
     def read_raw_data(self, addr):
-        high = bus.read_byte_data(self.address, addr);
-        low = bus.read_byte_data(self.address, addr+1);
+        high = bus.read_byte_data(self.address, addr)
+        low = bus.read_byte_data(self.address, addr+1)
 
-        value = ( (high << 8) | low);
+        value = ((high << 8) | low)
 
         # getting signed value
         if (value > 32768):
-            value = value - (32768 * 2);
+            value = value - (32768 * 2)
 
-        return value;
-
+        return value
 
     # Gets the corresponding raw data, converts it, rounds to the nearest
     # decimal place, and then returns.
+
     def getAccel_X(self, decimal):
-        return round((self.read_raw_data(ACCEL_XOUT_H)/16384.0), decimal);
+        return round((self.read_raw_data(ACCEL_XOUT_H)/16384.0), decimal)
 
     def getAccel_Y(self, decimal):
-        return round((self.read_raw_data(ACCEL_YOUT_H)/16384.0), decimal);
+        return round((self.read_raw_data(ACCEL_YOUT_H)/16384.0), decimal)
 
     def getAccel_Z(self, decimal):
-        return round((self.read_raw_data(ACCEL_ZOUT_H)/16384.0), decimal);
-
+        return round((self.read_raw_data(ACCEL_ZOUT_H)/16384.0), decimal)
 
     def getGyro_X(self, decimal):
-        return round((self.read_raw_data(GYRO_XOUT_H)/16384.0), decimal);
+        return round((self.read_raw_data(GYRO_XOUT_H)/16384.0), decimal)
 
     def getGyro_Y(self, decimal):
-        return round((self.read_raw_data(GYRO_YOUT_H)/16384.0), decimal);
+        return round((self.read_raw_data(GYRO_YOUT_H)/16384.0), decimal)
 
     def getGyro_Z(self, decimal):
-        return round((self.read_raw_data(GYRO_ZOUT_H)/16384.0), decimal);
-
+        return round((self.read_raw_data(GYRO_ZOUT_H)/16384.0), decimal)
 
     def getTemp(self, decimal):
-        return round((self.read_raw_data( ((float)TEMP_OUT_H)/340.00) + 36.53));
-
+        return round((self.read_raw_data(((float)TEMP_OUT_H)/340.00) + 36.53))
 
     # returns the acceleration data as a formatted, printable string.
+
     def acceleration_toString(self, decimal):
-    
-        ax = self.getAccel_X(decimal);
-        ay = self.getAccel_Y(decimal);
-        az = self.getAccel_Z(decimal);
 
-        toReturn = 'a_x: {0}, a_y: {1}, a_z: {2}'.format(ax, ay, az);
+        ax = self.getAccel_X(decimal)
+        ay = self.getAccel_Y(decimal)
+        az = self.getAccel_Z(decimal)
 
-        return toReturn;
+        toReturn = 'a_x: {0}, a_y: {1}, a_z: {2}'.format(ax, ay, az)
+
+        return toReturn
--- OutputComponent.py
+++ OutputComponent.py
@@ -12,68 +12,66 @@ you name it.
 import RPi.GPIO as GPIO
 import time
 
-class OutputComponent:
 
+class OutputComponent:
 
     # Constructor.
     def __init__(self, pin):
 
-        self.pin = pin;
+        self.pin = pin
 
-        GPIO.setmode(GPIO.BCM);
-        GPIO.setup(self.pin, GPIO.OUT);
+        GPIO.setmode(GPIO.BCM)
+        GPIO.setup(self.pin, GPIO.OUT)
 
-#        self.pulse = GPIO.PWM(self.pin, 1000);
-#        self.pulse.start(0);
-
-
+    # self.pulse = GPIO.PWM(self.pin, 1000);
+    # self.pulse.start(0);
 
     # In some cases, it might be more useful to have hardcoded functions for
     # turning the LED on or off. That is the purpose of the following two
     # functions:
+
     def toggleOn(self):
-        GPIO.output(self.pin, GPIO.HIGH);
+        GPIO.output(self.pin, GPIO.HIGH)
 
     def toggleOff(self):
-        GPIO.output(self.pin, GPIO.LOW);
+        GPIO.output(self.pin, GPIO.LOW)
 
     # Vibrates/Sounds the device for a length of time (sec).
     def step(self, duration):
-        GPIO.output(self.pin, GPIO.HIGH);
-        time.sleep(duration);
-        GPIO.output(self.pin, GPIO.LOW);
-        time.sleep(duration);
-    
-    # Vibrates/Sounds the device for a certain length of time continuously. Delay
-    # between the vibrations/sounds are set via the delay variable.
+        GPIO.output(self.pin, GPIO.HIGH)
+        time.sleep(duration)
+        GPIO.output(self.pin, GPIO.LOW)
+        time.sleep(duration)
+
+    # Vibrates/Sounds the device for a certain length of time continuously.
+    # Delay between the vibrations/sounds are set via the delay variable.
     def metronome(self, delay, duration):
 
-        timerEnd = time.time() + duration;
+        timerEnd = time.time() + duration
 
         while (time.time() < timerEnd):
-            self.step(delay);
-
+            self.step(delay)
 
     # DONT USE THIS UNLESS YOU ENABLE THE PWM OPTION IN INIT
+
     def breathe(self, duration):
 
         if (duration == 0):
-            return;
+            return
 
         while True:
 
-            delay = duration / (100/4) / 2;
+            delay = duration / (100/4) / 2
 
             for dutyCycle in range(0, 101, 4):
-                self.pulse.ChangeDutyCycle(dutyCycle);
-                sleep(delay);
+                self.pulse.ChangeDutyCycle(dutyCycle)
+                sleep(delay)
             for dutyCycle in reversed(range(0, 101, 4)):
-                self.pulse.ChangeDutyCycle(dutyCycle);
-                sleep(delay);
-
+                self.pulse.ChangeDutyCycle(dutyCycle)
+                sleep(delay)
 
     # Safely terminates the class. Good to call before closing script.
     def __destroy__(self):
-        #self.pulse.stop();
-        GPIO.output(self.pin, GPIO.HIGH);
-        GPIO.cleanup();
+        # self.pulse.stop();
+        GPIO.output(self.pin, GPIO.HIGH)
+        GPIO.cleanup()
